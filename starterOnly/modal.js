d11b3e962ea7a9cfd07f8c76f3dd5e3d3c662316
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-01-11 10:20:15 +0100

    Ajout de pattern au contrôle du champ Prénom

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,15 +11,17 @@ function editNav() {
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBg = document.querySelector(".bground");
 const crossModalBtn = document.querySelector("#cross-close-modal");
-const submitBtn = document.querySelectorAll(".btn-submit");
+const submitBtn = document.querySelector(".btn-submit");
 const thanksBg = document.querySelector(".bground-thanks");
 const crossThanksBtn = document.querySelector("#cross-close-thanks");
 const closeThanksBtn = document.querySelector(".btn-close-thanks");
+const reserveForm = document.getElementById("reserve-form");
 const formData = document.querySelectorAll(".formData");
 
 
 // launch modal event for registration
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//modalBtn.addEventListener("click", launchModal);
 
 // launch modal form
 function launchModal() {
@@ -65,50 +67,90 @@ function closeThanks() {
 // DOM Elements
 let firstName = document.getElementById("first");
 let lastName = document.getElementById("last");
-var email = document.getElementById("email");
+let email = document.getElementById("email");
 let birthDay = document.getElementById("birthdate");
 let tournamentQty = document.getElementById("quantity");
-let newYork = document.getElementById("location1").checked;
-let sanFrancisco = document.getElementById("location2").checked;
-let seatle = document.getElementById("location3").checked;
-let chicago = document.getElementById("location4").checked;
-let boston = document.getElementById("location5").checked;
-let porland = document.getElementById("location6").checked;
+const newYork = document.getElementById("location1").checked;
+const sanFrancisco = document.getElementById("location2").checked;
+const seatle = document.getElementById("location3").checked;
+const chicago = document.getElementById("location4").checked;
+const boston = document.getElementById("location5").checked;
+const porland = document.getElementById("location6").checked;
 let checkBox = document.getElementById("checkbox1");
 let errorMessage = document.getElementById("errorname");
 
+/* Caractères unicode: À, É, Ä, Ç, È, É, Ê, Ë, Î, Ï, Ô, Ö, Ù, Û, Ü, à, á, â, ä, æ, ç, è, é, ê, ë, ï, ô, ö, ù, û */
+/* unicodeCars = \u00c0-\u00c2\u00c4\u00c6-\u00cf\u00d4\u00d6\u00d9-\u00dc\u00e0-\u00f6\u00f9-\u00fc; */
 
-firstName.addEventListener("blur", function() {
-  if (firstName.value == "" || firstName.value.length < 2) {
+patternText = /^[a-zA-Z\u00c0-\u00c2\u00c4\u00c6-\u00cf\u00d4\u00d6\u00d9-\u00dc\u00e0-\u00f6\u00f9-\u00fc\s-]+$/g;
+patternEmail = /([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)/g;
+patternDate = new RegExp("^\d{2}/\d{2}/\d{4}$", "g");
+patternNumber = /^\d{1,2}$/g;
+
+//  firstName.setCustomValidity("Gestion des messages d'erreur");
+
+firstName.addEventListener("input", function() {
+  if (firstName.value == "" || firstName.value.length < 2) {  //if empty string or less than 2 characters
     errorMessage.style.visibility = "visible";  
-    errorMessage.innerHTML = "Veuillez entrez un Prénom valide";  
+    errorMessage.innerHTML = "Veuillez entrer au moins 2 caractères pour le champ du prénom.";
+  //  firstName.setCustomValidity("Veuillez entrer au moins 2 caractères pour le champ du prénom.");
     firstName.focus(); 
-  } else {
+  }  else {
     errorMessage.style.visibility = "hidden";  
     errorMessage.innerHTML = ""; 
   }
 });
 
-document.getElementById("submit").addEventListener("click", function(e) { 
-  if (firstName.value == "" || firstName.value.length < 2) {
+firstName.addEventListener("blur", function() {
+  if (firstName.value == "" || firstName.value.length < 2) {  //if empty string or less than 2 characters
     errorMessage.style.visibility = "visible";  
-    errorMessage.innerHTML = "Veuillez entrez un Prénom valide";  
+    errorMessage.innerHTML = "Veuillez entrer au moins 2 caractères pour le champ du prénom.";
     firstName.focus(); 
-    e.preventDefault();
-  } else {
-    launchThanks();
-    e.preventDefault();
   }
-
+  
+  if (!patternText.test(firstName.value)) {  //if the user types a wrong character
+    errorMessage.style.visibility = "visible";  
+    errorMessage.innerHTML = "Seuls les caractères alphabétiques, les traits d'union et espaces sont autorisés.";
+    firstName.focus(); 
+  } 
+  
+  if (!(firstName.value == "" || firstName.value.length < 2) && patternText.test(firstName.value)) {
+    console.log("its ok: " + firstName.value);
+    console.log(patternText.test(firstName.value));
+    errorMessage.style.visibility = "hidden";  
+    errorMessage.innerHTML = "";   //cancel error message
+  }
 });
 
 
-/*
-if ((firstName.value == "") || (firstName.value.length < 2)) {
-  console.log(firstName.value.length);
-  firstName.setCustomValidity("Veuillez entrer 3 caractères ou plus pour le champ du prénom.");
-} else {
-  firstName.setCustomValidity("");
+submitBtn.addEventListener("click", function(e) { 
+  if (firstName.value == "" || firstName.value.length < 2) { //if empty string or less than 2 characters
+    errorMessage.style.visibility = "visible";  
+    errorMessage.innerHTML = "Veuillez entrer au moins 2 caractères pour le champ du prénom.";
+    firstName.focus();
+    e.preventDefault();
+  } else if (!patternText.test(firstName.value)) {  //if the user types a wrong character
+    errorMessage.style.visibility = "visible";  
+    errorMessage.innerHTML = "Seuls les caractères alphabétiques, les traits d'union et espaces sont autorisés.";
+    firstName.focus(); 
+    e.preventDefault();
+  } else {  //display the modal thanks
+    console.log(patternText.test(firstName.value));
+    //   validateForm();
+    launchThanks();
+    reserveForm.reset();
+    e.preventDefault();
+  }
+  
+});
+
+function validateForm() {
+  let prenom = firstName.value;
+  let nom = lastName.value;
+  let adrEmail = email.value;
+  console.log(prenom);
+  console.log(nom);
+  console.log(adrEmail);
 }
-*/
+
 
