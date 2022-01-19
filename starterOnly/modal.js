74a2c891b3a60ebc43f9718f5944df5524562969
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-01-19 16:15:04 +0100

    renommage des fonctions en fonctions fléchées et test de récupération des données.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,6 +31,7 @@ const chicago = document.getElementById("location4");
 const boston = document.getElementById("location5");
 const porland = document.getElementById("location6");
 const agree = document.getElementById("checkbox1");
+const nextEvents = document.getElementById("checkbox2");
 const errorFirstName = document.getElementById("error-fname");
 const errorLastName = document.getElementById("error-lname");
 const errorEmail = document.getElementById("error-email");
@@ -38,6 +39,8 @@ const errorBirthday = document.getElementById("error-birthday");
 const errorTournament = document.getElementById("error-tournament");
 const errorLocation = document.getElementById("error-location");
 const errorAgreement = document.getElementById("error-agreement");
+
+let chosenLocation = "";
 
 /* Caractères unicode: À, É, Ä, Ç, È, É, Ê, Ë, Î, Ï, Ô, Ö, Ù, Û, Ü, à, á, â, ä, æ, ç, è, é, ê, ë, ï, ô, ö, ù, û */
 /* unicodeCars = \u00c0-\u00c2\u00c4\u00c6-\u00cf\u00d4\u00d6\u00d9-\u00dc\u00e0-\u00f6\u00f9-\u00fc; */
@@ -85,7 +88,7 @@ function closeThanks() {
 
 
 /*** Contrôles des champs prénom ou nom à la saisie ***/
-function ControlNameSeizure(field, error) {
+ControlNameSeizure = (field, error) => {
   if (field.value != "" || field.value.length >= 2 || field.value.length <= 30) {  //if empty string or characters are between 2 and 30
     error.style.visibility = "hidden";  
     error.innerHTML = ""; 
@@ -93,17 +96,17 @@ function ControlNameSeizure(field, error) {
 }
 
 /*** Contrôle du champ prénom à la saisie ***/
-firstName.addEventListener("input", function() {
+firstName.addEventListener("input", () => {
   ControlNameSeizure(firstName, errorFirstName);
 });
 
 /*** Contrôle du champ nom à la saisie ***/
-lastName.addEventListener("input", function() {
+lastName.addEventListener("input", () => {
   ControlNameSeizure(lastName, errorLastName);
 });
 
 /*** Validation du prénom ou du nom ***/
-function validateName(field, error, event) {
+validateName = (field, error, event) => {
   const patternTextResult = patternText.test(field.value.toString());
   
   if (field.value == "" || field.value.length < 2 || field.value.length > 30) { //if empty string or characters are not between 2 and 30
@@ -128,7 +131,7 @@ function validateName(field, error, event) {
 
 
 /*** Contrôle à la saisie ***/
-function ControlSeizure(field, error) {
+ControlSeizure = (field, error) => {
   if (!(field.value == "")) {  //if not empty string 
     error.style.visibility = "hidden";  
     error.innerHTML = ""; 
@@ -136,12 +139,12 @@ function ControlSeizure(field, error) {
 }
 
 /*** Contrôle du champ Email à la saisie ***/
-email.addEventListener("input", function() {
+email.addEventListener("input", () => {
   ControlSeizure(email, errorEmail);
 });
 
 /*** Validation du champ Email ***/
-function validateEmail(field, error, event) {
+validateEmail = (field, error, event) => {
   const patternEmailResult = patternEmail.test(String(field.value).toLowerCase());
   
   if (field.value == "" || patternEmailResult == false) { 
@@ -161,12 +164,12 @@ function validateEmail(field, error, event) {
 
 /*** Contrôle de la date de naissance ***/
 /*** Contrôle de la date de naissance à la saisie ***/
-birthday.addEventListener("input", function() {
+birthday.addEventListener("input", () => {
   ControlSeizure(birthday, errorBirthday);
 });
 
 /*** Validation de la date de naissance ***/
-function validateBirthday(field, error, event) {
+validateBirthday = (field, error, event) => {
  // const patternBirthdayResult = patternBirthday.test(field.value.toString());
   
   if (field.value == "" || field.value.length != 10) { 
@@ -185,12 +188,12 @@ function validateBirthday(field, error, event) {
 
 
 /*** Contrôle du nombre de tournois à la saisie ***/
-tournamentQty.addEventListener("input", function() {
+tournamentQty.addEventListener("input", () => {
   ControlSeizure(tournamentQty, errorTournament);
 });
 
 /*** Validation du nombre de tournois ***/
-function validateTournamentQty(field, error, event) {
+validateTournamentQty = (field, error, event) => {
   const patternNumberResult = patternNumber.test(field.value.toString());
   
   if (field.value == "") { 
@@ -215,16 +218,14 @@ function validateTournamentQty(field, error, event) {
 
 
 /*** Validation du choix du lieu ***/
-function validateLocation(_field, error, event){
-  let valeur="";
-
+validateLocation = (_field, error, event) => {
   for(let i = 0; i < locationArray.length; i++){
     if(locationArray[i].checked) {
-      valeur = locationArray[i].value;
+      chosenLocation = locationArray[i].value;
     }
   }
 
-  if (valeur == "") {
+  if (chosenLocation == "") {
     error.style.visibility = "visible";  
     error.innerHTML = "Veuillez choisir un lieu.";
     event.preventDefault();
@@ -237,42 +238,42 @@ function validateLocation(_field, error, event){
 }
 
 /*** Ecoute des boutons radio des lieux ***/
-newYork.addEventListener("click", function() {
+newYork.addEventListener("click", () => {
   if(newYork.checked) {
     errorLocation.style.visibility = "hidden";  
     errorLocation.innerHTML = "";
   }
 });
 
-sanFrancisco.addEventListener("click", function() {
+sanFrancisco.addEventListener("click", () => {
   if(sanFrancisco.checked) {
     errorLocation.style.visibility = "hidden";  
     errorLocation.innerHTML = "";
   }
 });
 
-seatle.addEventListener("click", function() {
+seatle.addEventListener("click", () => {
   if(seatle.checked) {
     errorLocation.style.visibility = "hidden";  
     errorLocation.innerHTML = "";
   }
 });
 
-chicago.addEventListener("click", function() {
+chicago.addEventListener("click", () => {
   if(chicago.checked) {
     errorLocation.style.visibility = "hidden";  
     errorLocation.innerHTML = "";
   }
 });
 
-boston.addEventListener("click", function() {
+boston.addEventListener("click", () => {
   if(boston.checked) {
     errorLocation.style.visibility = "hidden";  
     errorLocation.innerHTML = "";
   }
 });
 
-porland.addEventListener("click", function() {
+porland.addEventListener("click", () => {
   if(porland.checked) {
     errorLocation.style.visibility = "hidden";  
     errorLocation.innerHTML = "";
@@ -281,7 +282,7 @@ porland.addEventListener("click", function() {
 
 
 /*** Validation de l'accord des conditions ***/
-function validateAgreement(_field, error, event){
+validateAgreement = (_field, error, event) => {
   if (agree.checked == false) {
     error.style.visibility = "visible";  
     error.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
@@ -295,7 +296,7 @@ function validateAgreement(_field, error, event){
 }
 
 /**Ecoute du check d'accord des conditions ***/
-agree.addEventListener("click", function() {
+agree.addEventListener("click", () => {
   if(agree.checked) {
     errorAgreement.style.visibility = "hidden";  
     errorAgreement.innerHTML = "";
@@ -304,7 +305,7 @@ agree.addEventListener("click", function() {
 
 
 /*** Contrôle du bouton submit ***/
-submitBtn.addEventListener("click", function(e) { 
+submitBtn.addEventListener("click", (e) => { 
   if (validateName(firstName, errorFirstName, e) && 
       validateName(lastName, errorLastName, e) && 
       validateEmail(email, errorEmail, e) && 
@@ -312,8 +313,8 @@ submitBtn.addEventListener("click", function(e) {
       validateTournamentQty(tournamentQty, errorTournament, e) && 
       validateLocation(locationArray, errorLocation, e) && 
       validateAgreement(agree, errorAgreement, e)) {
-    //   validateForm();
-     launchThanks();  //display the modal thanks
+    validateForm();
+    launchThanks();  //display the modal thanks
     reserveForm.reset();  //reset the form
     e.preventDefault();   //prevent the modal thank to close automatically
   }
@@ -322,13 +323,24 @@ submitBtn.addEventListener("click", function(e) {
 
 
 /*** Test de récupération des données ***/
-function validateForm() {
+validateForm = () => {
   const prenom = firstName.value;
   const nom = lastName.value;
   const adrEmail = email.value;
-  console.log(prenom);
-  console.log(nom);
-  console.log(adrEmail);
+  const anniversaire = birthday.value;
+  const nbreTournois = tournamentQty.value;
+  const lieu = chosenLocation;
+  const accord = agree.checked;
+  const prochainsEvts = nextEvents.checked;
+
+  console.log("Prénom :"+prenom);
+  console.log("Nom: "+nom);
+  console.log("E-mail: "+adrEmail);
+  console.log("Anniversaire: "+anniversaire);
+  console.log("Nombre de tournois: "+nbreTournois);
+  console.log("Lieu choisi: "+lieu);
+  console.log("Accord: "+accord);
+  console.log("Prochains évènements: "+prochainsEvts);
 }
 
 
